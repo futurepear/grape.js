caef3cc71f8cce04ff262058dbc0ca02f828f023
Adrian Alupoaei <99107938+futurepear@users.noreply.github.com>
2022-10-28 11:33:00 -0700

    Create subserver.js

---
+++ subserver.js
@@ -0,0 +1,16 @@
+//example using Express JS 
+
+const express = require('express');
+const http = require('node:http');
+const app = express();
+
+//create a http server and pass the express app instance into it, all requests will be done automatically
+const server = http.createServer(app);
+
+//set up a branch with the http server to recieve connections
+const grape = require("./grape").setupBranch(server);
+
+//express routing
+app.get('/', (req, res) => {
+  res.send("foo");
+});

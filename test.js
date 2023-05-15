7fcdfd70c46bc12dba9da912f24e2aa4faf54ba7
Future Pear <99107938+futurepear@users.noreply.github.com>
2023-05-15 08:52:34 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 const net = require("node:net");
+const crypto = require("crypto");
+
+let server = net.createServer();
+
+server.on("connection", (socket) => {
+  socket.on("data", (data) => {
+    let str = data.toString();
+    
+    //try decrypting https here
+    str = crypto.publicDecrypt("PUT YOUR KEY THING HERE!!!!!!", str);
+    //a
+    
+    console.log(str);
+  });
+});
+
+server.listen(3000);
